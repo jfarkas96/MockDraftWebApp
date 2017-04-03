bf8374947a7a7b8140c0b25087bdec37863d869a
Justin Farkas <jfarkas3@uncc.edu>
2017-04-03 15:50:04 +0000

    added a new player view and changed the create functionality in the players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,2 +1,17 @@
 class PlayersController < ApplicationController
+    def new
+    end
+    
+    def create
+        @player = Player.new(player_params)
+        
+        @player.save
+        redirect_to @article
+    end
 end
+
+private
+    def player_params 
+        params.require(:player).permit(:name, :college, :position, :grade, :height, :weight, :picture_link)
+    end
+
