e11ae1c7058ec51f10fe2f0ee3fc44e31dfb13f4
Justin Farkas <jfarkas3@uncc.edu>
2017-04-03 20:05:17 +0000

    Added index for teams, updated links on other views, styling changes

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,24 @@
 class TeamsController < ApplicationController
+  def new
+  end
+  
+  def index
+    @teams = Team.all
+  end
+  
+  def show
+    @team = Team.find(params[:id])
+  end
+  
+  def create
+    @team = Team.new(team_params)
+    
+    @team.save
+    redirect_to @team
+  end
 end
+
+private
+  def team_params
+    params.require(:team).permit(:name, :needs, :round1, :round2, :round3, :round4, :round5, :round6, :round7, :picture_link)
+  end
